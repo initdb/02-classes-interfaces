7f628c7456174a3b32b0ea123ea8d905672d0684
sEITbtschw <benedict.schwind@stud.fh-rosenheim.de>
2018-10-11 14:00:47 +0200

    add interface functions

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-_This is an assignment to the class [Programmieren 3](https://hsro-inf-prg3.github.io) at the [University of Applied Sciences Rosenheim](http://www.fh-rosenheim.de)._
+`__````__`_This is an assignment to the class [Programmieren 3](https://hsro-inf-prg3.github.io) at the [University of Applied Sciences Rosenheim](http://www.fh-rosenheim.de)._
 
 # Assignment 2: Classes and interfaces revisited
 
--- src/main/java/de/thro/inf/prg3/a02/SimpleListImpl.java
+++ src/main/java/de/thro/inf/prg3/a02/SimpleListImpl.java
@@ -4,8 +4,31 @@ package de.thro.inf.prg3.a02;
  * @author Peter Kurfer
  * Created on 10/6/17.
  */
-public class SimpleListImpl implements SimpleList {
+public class SimpleListImpl implements SimpleList
+{
+	Element head;
 
-	// TODO: Implement the required methods.
+    private static class Element
+    {
+        Object item;
+        Element next;
+    }
 
+    @Override
+    public void add(Object o)
+    {
+
+    }
+
+    @Override
+    public int size()
+    {
+        return 0;
+    }
+
+    @Override
+    public SimpleList filter(SimpleFilter filter)
+    {
+        return null;
+    }
 }
